73154fbdb8e2a44c42e40ebdf53ed45ed29ba1c2
happiness <happinesskhalan@gmail.com>
2025-02-26 00:00:28 +0300

    funciton practice

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Follow along with the examples here
+function sayHello(){
+    console.log("Hello!");
+}
+
+function sayHelloToGuadalupe(){
+    console.log("Hello, Guadalupe!")
+}
+function sayHelloToLiz(){
+    console.log("Hello, Liz!")
+}
+
+function sayHelloToSamipi(){
+    console.log("Hello, Samipi!")
+}
+
+
+
+sayHelloToGuadalupe();
+sayHelloToSamipi();
+sayHello();
+sayHelloToLiz();
+
+function doSomething(thing){
+    console.log(thing)
+}
+
+doSomething("anything")
+
+function sayHelloTo(firstName){
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Guadelupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+
+function say(greeting, secondName){
+    console.log(`${greeting}, ${secondName}!`)
+}
+
+
+say("Julio", "Goodbye");
